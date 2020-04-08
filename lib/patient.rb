ff46078e71518a097b8737e46aa5302310322a77
John Stewart <jnicklausstewart@yahoo.com>
2020-04-08 14:47:13 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -20,8 +20,8 @@ class Patient
     appointments.collect {|appointment| appointment.doctor}
   end
 
-  def new_appointment(patient, doctor, date)
-    Appointment.new(patient, date, doctor)
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
   end
 
 end 
